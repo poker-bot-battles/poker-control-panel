2cfed7eeb59698b9d1b53fe5d253bdad4acd6bab
Christian Alexander Hetling <chhetling@gmail.com>
2022-12-07 18:44:40 +0100

    added pass

--- control_panel.py
+++ control_panel.py
@@ -3,40 +3,64 @@ import json
 import streamlit as st
 
 # button to connect to websocket
-ws = websocket.WebSocket()
-try:
-        ws.connect("ws://localhost:8765")
-        st.write("Connected to websocket")
-except Exception as e:
-    st.write("Connection failed error: ", e)
+def check_password():
+    """Returns `True` if the user had the correct password."""
 
-# check connection
-if st.button("Check connection"):
-    st.write("Connection status: ", ws.connected)
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
 
-# check connected clients
-if st.button("Check connected clients"):
-    ws.send(json.dumps({"cmd": "get clients"}))
-    result = ws.recv()
-    st.write("Connected clients: ", json.loads(result)['clients'])
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
 
-if st.button("Load"):
+if check_password():
+    ws = websocket.WebSocket()
     try:
-        ws.send(json.dumps({"cmd": "load"}))
-        st.write("Load message sent")
+            ws.connect("wss://socket-p45vitbqra-ew.a.run.app")
+            st.write("Connected to websocket")
     except Exception as e:
-        st.write("Load failed error: ", e)
+        st.write("Connection failed error: ", e)
 
-# button to send start message to websocket
+    # check connection
+    if st.button("Check connection"):
+        st.write("Connection status: ", ws.connected)
 
-if st.button("Start"):
-    try:
-        ws.send(json.dumps({"cmd": "start"}))
-        st.write("Start message sent")
-    except Exception as e:
-        st.write("Start failed error: ", e)
+    # check connected clients
+    if st.button("Check connected clients"):
+        ws.send(json.dumps({"cmd": "get clients"}))
+        result = ws.recv()
+        st.write("Connected clients: ", json.loads(result)['clients'])
 
+    if st.button("Load"):
+        try:
+            ws.send(json.dumps({"cmd": "load"}))
+            st.write("Load message sent")
+        except Exception as e:
+            st.write("Load failed error: ", e)
 
+    # button to send start message to websocket
 
-
-
+    if st.button("Start"):
+        try:
+            ws.send(json.dumps({"cmd": "start"}))
+            st.write("Start message sent")
+        except Exception as e:
+            st.write("Start failed error: ", e)
